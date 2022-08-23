2c426702ce68deba60048780b4383cc660a9a365
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-08-23 17:55:45 +0530

    import java.util.*;
    class sol{
        public static void main(String args[])
        {
            Scanner sc = new Scanner(System.in);
            int ch = sc.next().charAt(0);
            // int ascii=ch;
            System.out.print(ch);
        }
    }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class sol{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int ch = sc.next().charAt(0);
+        // int ascii=ch;
+        System.out.print(ch);
+    }
+}
