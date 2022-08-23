5c84a9ba072162d94ffd5fec42adaeb2613e2c8f
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-08-23 18:33:38 +0530

    import java.util.*;
    class sol{
        public static void main(String args[])
        {
            Scanner sc = new Scanner(System.in);
            double a = sc.nextDouble(),b = sc.nextDouble();
            double ans = (a+b)/2;
            System.out.format("%.4f",ans);
        }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class sol{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble(),b = sc.nextDouble();
+        double ans = (a+b)/2;
+        System.out.format("%.4f",ans);
+    }
+}
