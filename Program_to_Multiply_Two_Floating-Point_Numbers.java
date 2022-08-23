4c33ecbbf19d5c23f88e8c19a0407ae3d9c8eb54
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-08-23 17:59:28 +0530

    import java.util.*;
    class sol{
        public static void main(String args[]){
            Scanner sc = new Scanner(System.in);
            double a = sc.nextDouble(),b = sc.nextDouble();
            System.out.format("%.2f",a*b);
        }
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class sol{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble(),b = sc.nextDouble();
+        System.out.format("%.2f",a*b);
+    }
+}
