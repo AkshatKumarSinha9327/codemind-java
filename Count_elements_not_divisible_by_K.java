c314dc2c8b9ff5e12393ef2b1863078d84c43a67
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-09-24 11:44:40 +0530

    import java.util.*;
    class sol{
        public static void main(String args[])
        {
            Scanner sc = new Scanner(System.in);
            int n = sc.nextInt(),k=sc.nextInt();
            int [] arr = new int[n];
            int c=0;
            for(int i=0;i<n;i++)
            {
                arr[i] = sc.nextInt();
                if (arr[i]%k!=0){
                    c++;
                }
            }
            System.out.print(c);
            
        }
    }

---
+++ Count_elements_not_divisible_by_K.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class sol{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(),k=sc.nextInt();
+        int [] arr = new int[n];
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = sc.nextInt();
+            if (arr[i]%k!=0){
+                c++;
+            }
+        }
+        System.out.print(c);
+        
+    }
+}
