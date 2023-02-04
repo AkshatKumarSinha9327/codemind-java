5653aab9af958fca7b722c3a4e160c6ec6abb861
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2023-02-04 11:27:33 +0530

    import java.util.*;
    class sol
    {
        public static void main(String args[])
        {
            Scanner sc = new Scanner(System.in);
            int n = sc.nextInt(),k = sc.nextInt();
            int arr[] = new int[n];
            for(int i = 0 ; i < n ;i++)
            {
                arr[i] = sc.nextInt();
            }
            // k = k%n;
            for(int i = k ; i < n ;i++)
            {
                System.out.print(arr[i]+" ");
            }
            for(int i = 0 ; i < k ;i++)
            {
                System.out.print(arr[i]+" ");
            }
        }
    }

---
+++ Left_Rotate_an_Array_by_K.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+class sol
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(),k = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0 ; i < n ;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        // k = k%n;
+        for(int i = k ; i < n ;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        for(int i = 0 ; i < k ;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
